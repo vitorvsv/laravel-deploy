69d25c86aa484179b6d0cd1a808011ca9aa48dab
Vitor Soares Vian <vitorsoaresvian@gmail.com>
2025-09-29 16:42:04 -0300

    Change home route message

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -6,6 +6,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return json_encode(['message' => 'Home route']);
+        return json_encode(['message' => 'Hello World!']);
     }
 }
