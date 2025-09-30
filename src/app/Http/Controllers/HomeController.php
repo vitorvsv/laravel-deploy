46a90e9d470503b67d2b5c8f4053baa925f81d1d
Vitor Soares Vian <vitorsoaresvian@gmail.com>
2025-09-30 11:17:00 -0300

    Change home message for hello from kubernetes

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -6,6 +6,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return json_encode(['message' => 'Hello World!']);
+        return json_encode(['message' => 'Hello World from Kubernetes!']);
     }
 }
